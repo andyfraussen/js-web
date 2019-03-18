7a79ba7289d9f719461036e3292bc812802cf964
Andy Fraussen <fraussen.andy@gmail.com>
2019-03-18 09:19:47 +0100

    ex 1

--- exercise-serie-1/exercise-1/script.js
+++ exercise-serie-1/exercise-1/script.js
@@ -1,5 +1,21 @@
 //Check to see if script is linked properly.
 console.log("This script is linked properly!")
-
-
 //Write your JS code here...
+
+document.getElementById('blue').onclick = changeBlue;
+
+function changeBlue() {
+    document.getElementById("square").style.backgroundColor = "blue";
+}
+
+document.getElementById('red').onclick = changeRed;
+
+function changeRed() {
+    document.getElementById("square").style.backgroundColor = "red";
+}
+
+document.getElementById('green').onclick = changeGreen;
+
+function changeGreen() {
+    document.getElementById("square").style.backgroundColor = "green";
+}
