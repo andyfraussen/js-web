d6d0fbaabf9171ab1ab7ce456d78ae516f403c26
Andy Fraussen <fraussen.andy@gmail.com>
2019-03-19 11:49:21 +0100

    oef 7

--- exercise-serie-1/exercise-7/script.js
+++ exercise-serie-1/exercise-7/script.js
@@ -1,5 +1,12 @@
 //Check to see if script is linked properly.
 console.log("This script is linked properly!")
-
-
 //Write your JS code here...
+
+
+function reset() {
+    var float = document.getElementById("knopje");
+    var x = Math.floor(Math.random() * 600);
+    var y = Math.floor(Math.random() * 600);
+    float.style.top = y + 'px';
+    float.style.left = x + 'px';
+}
